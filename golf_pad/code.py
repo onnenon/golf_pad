febcebbdcaadc1bd9627a1ae36762a5858a22bea
Stephen Onnen <stephen.onnen@gmail.com>
2021-10-10 21:16:12 -0500

    Cleanup

--- golf_pad/code.py
+++ golf_pad/code.py
@@ -5,12 +5,20 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from digitalio import DigitalInOut, Direction
 
-# Turn the LED on
-led = DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
-led.value = True
-
-kbd = Keyboard(usb_hid.devices)
+keymap = [
+    Keycode.A,
+    Keycode.B,
+    Keycode.C,
+    Keycode.D,
+    Keycode.E,
+    Keycode.F,
+    Keycode.G,
+    Keycode.H,
+    Keycode.I,
+    Keycode.J,
+    Keycode.K,
+    Keycode.L,
+]
 
 pins = [
     board.GP0,
@@ -18,16 +26,26 @@ pins = [
     board.GP2,
     board.GP3,
     board.GP4,
+    board.GP5,
+    board.GP6,
+    board.GP7,
+    board.GP8,
+    board.GP9,
+    board.GP10,
+    board.GP11,
 ]
 
-keymap = [Keycode.A, Keycode.B, Keycode.C, Keycode.D, Keycode.E]
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+led.value = True
+
+keyboard = Keyboard(usb_hid.devices)
 
 switches = keypad.Keys(pins, value_when_pressed=False, pull=True)
 
 while True:
-    event = switches.events.get()
-    if event:
+    if event := switches.events.get():
         if event.pressed:
-            kbd.press(keymap[event.key_number])
-        if event.released:
-            kbd.release(keymap[event.key_number])
+            keyboard.press(keymap[event.key_number])
+        else:
+            keyboard.release(keymap[event.key_number])
